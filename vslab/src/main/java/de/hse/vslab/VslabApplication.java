49615391dc7eeafa44e2d73d64d752f0353f68f4
Maximilian Fink <mail@maximilian-fink.de>
2023-06-18 19:07:56 +0200

    init

---
+++ vslab/src/main/java/de/hse/vslab/VslabApplication.java
@@ -0,0 +1,13 @@
+package de.hse.vslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VslabApplication.class, args);
+	}
+
+}
---
+++ vslab/src/test/java/de/hse/vslab/VslabApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hse.vslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VslabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
